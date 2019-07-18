c1fc98968ee3a1254d9ace388cacda497ead531b
macxwhale <macxwhale@gmail.com>
2019-07-18 10:59:27 +0300

    "Implemented Recycler View and Books Adapter"

--- app/src/main/java/com/example/books/BookListActivity.java
+++ app/src/main/java/com/example/books/BookListActivity.java
@@ -1,21 +1,28 @@
 package com.example.books;
 
 import android.os.AsyncTask;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.app.SearchManager;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class BookListActivity extends AppCompatActivity {
+public class BookListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private ProgressBar mLoadingProgress;
     private RecyclerView rvBooks;
@@ -42,6 +49,32 @@ public class BookListActivity extends AppCompatActivity {
             Log.d("Error", e.getMessage());
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        try {
+            URL bookUrl = ApiUtil.buildURl(query);
+            new BooksQueryTask().execute(bookUrl);
+        }
+        catch (Exception e) {
+            Log.d("Error", e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 
     public class BooksQueryTask extends AsyncTask<URL, Void, String> {
